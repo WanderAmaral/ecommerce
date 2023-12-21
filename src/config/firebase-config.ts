4e1d24cae72630ac2de762f5136fdb7b9a07c148
Wander Amaral <wanderamaral013@gmail.com>
2023-12-21 20:45:06 -0300

    feat: add script that populates firestore

---
+++ src/config/firebase-config.ts
@@ -0,0 +1,14 @@
+import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTWfSnkr_a23YFeGb4ONmHlCFIikumY7k",
+  authDomain: "club-ecommerce-313da.firebaseapp.com",
+  projectId: "club-ecommerce-313da",
+  storageBucket: "club-ecommerce-313da.appspot.com",
+  messagingSenderId: "967700354438",
+  appId: "1:967700354438:web:27ce6090a13d6a231a8807",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
