b249a5d9f994e42053e7fb4548cf0f1d2c744921
Wander Amaral <wanderamaral013@gmail.com>
2023-12-22 22:37:53 -0300

    feat: add validator to form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,60 +1,67 @@
+"use client";
+
 import React from "react";
 import Button from "../components/button";
-import {BsGoogle} from 'react-icons/bs'
-import {FiLogIn} from 'react-icons/fi'
+import { BsGoogle } from "react-icons/bs";
+import { FiLogIn } from "react-icons/fi";
 import CustomInputContainer from "./components/custom-input";
 import { useForm } from "react-hook-form";
 
-
-interface Users {
-  email: string,
-  password: string
-}
-
-
 const LoginPage = () => {
-  const {register, formState: {errors}, handleSubmit} = useForm<Users>()
-  
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const handleClickSubmit = (data: any) => {
-    console.log({data})
-  }
+  const onSubmit = (data: any) => {
+    console.log({ data });
+  };
 
-  console.log({errors})
   return (
-    
     <div className="h-full flex items-center justify-center mt-40">
       {/* LoginContainer */}
       <div className="flex flex-col items-center w-96">
         {/* LoginContent */}
 
-        <p className=" text-texto-dark font-semibold mb-5 text-rem">
+        <p className="text-texto-dark font-semibold mb-5 text-rem">
           Entre com a sua conta
         </p>
         {/* LoginHeadline */}
         {/* LoginSubtitle */}
-        <Button startIcon={<BsGoogle size={18}/>}>Entrar com o Google</Button>
-        <p className="text-primary w-full p-3 mb-8  text-center font-medium  border-b border-black">
+        <Button startIcon={<BsGoogle size={18} />}>Entrar com o Google</Button>
+        <p className="text-primary w-full p-3 mb-8 text-center font-medium border-b border-black">
           ou entre com seu email
         </p>
 
         {/* LoginInputContainer */}
-        <div className="w-full mb-5">
-          <p className=" font-semibold mb-5">Usuário</p>
-          <CustomInputContainer placeholder="Digite seu email" {...register('email', {required: true})}/>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+          <div className="w-full mb-5">
+            <p className="font-semibold mb-5">Usuário</p>
+            <CustomInputContainer
+            
+              placeholder="Digite seu email"
+              {...register("email", { required: true })}
+            />
+            {/* {errors.email && <p className="text-red-500">Campo obrigatório</p>} */}
+          </div>
 
-        <div className="w-full">
-          <p className="font-semibold mb-5">Senha</p>
-          <CustomInputContainer placeholder="Digite sua senha" {...register('password', {required: true})}/>
-        </div>
-        {/* End LoginInputContainer */}
+          <div className="w-full mb-5">
+            <p className="font-semibold mb-5">Senha</p>
+            <CustomInputContainer
+              placeholder="Digite sua senha"
+              {...register("password", { required: true })}
+            />
+            {/* {errors.password && <p className="text-red-500">Campo obrigatório</p>} */}
+          </div>
 
-        <Button startIcon={<FiLogIn size={18}/>} onClick={() => handleSubmit(handleClickSubmit)()}>Entrar</Button>
+          {/* End LoginInputContainer */}
+          <Button
+            type="submit"
+            startIcon={<FiLogIn size={18} />}
+          >
+            Entrar
+          </Button>
+        </form>
         {/* Add your button component or Tailwind CSS classes here */}
       </div>
     </div>
-    
   );
 };
 
