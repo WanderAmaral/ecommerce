2d71259779d3edf0d4903083d0f9d50310cfe92d
Wander Amaral <wanderamaral013@gmail.com>
2023-12-24 13:57:13 -0300

    commit

--- src/app/components/header.components.tsx
+++ src/app/components/header.components.tsx
@@ -3,14 +3,14 @@ import {BsCart} from 'react-icons/bs'
 
 const Header = () => {
     return (
-      <div className="  bg-background-dark w-full flex justify-between p-4 text-white"> {/* Header-container */}
+      <div className="  bg-background-dark w-full flex justify-between p-4 text-white shadow-xl"> {/* Header-container */}
         <div className="flex items-center"> {/* Header-items-container */}
           <h1 className="text-lg mr-4">Ecommerce</h1> {/* header-title */}
         </div>
         <div className="flex items-center gap-10"> {/* Header-items */}
           <Link href={"/login"}><div className="font-semibold text-sm">Login</div></Link> {/* Header-item */}
           <div className="font-semibold text-sm">Explorar</div> {/* Header-item */}
-          <div className="font-semibold text-sm">Conta</div> {/* Header-item */}
+          <Link href={"create-login"}><div className="font-semibold text-sm">Criar Conta</div></Link> {/* Header-item */}
           <div className="flex font-semibold text-sm"><BsCart size={20}/><p className='ml-1'>5</p></div> {/* Header-item */}
         </div>
       </div>
