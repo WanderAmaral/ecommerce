9a38b5dbe4803b96cb8f69c23f18d03628b7ec6e
Wander Amaral <wanderamaral013@gmail.com>
2023-12-26 10:13:33 -0300

    feat: create error in create user

--- src/app/create-login/page.tsx
+++ src/app/create-login/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { FiLogIn } from "react-icons/fi";
 import { useForm } from "react-hook-form";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { db } from "@/config/firebase-config";
-import { auth } from "@/config/firebase-config";
+import { auth, db } from "@/config/firebase-config";
+import {
+  AuthError,
+  createUserWithEmailAndPassword,
+  AuthErrorCodes,
+} from "firebase/auth";
 import { addDoc, collection } from "firebase/firestore";
-import { onSubmit } from "@/actions/create-user";
 
 import CustomInput from "../components/custom-input";
 import Button from "../components/button";
@@ -23,10 +25,33 @@ const CreateLogin = () => {
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors },
   } = useForm<SignUpForm>();
 
-  
+  const onSubmit = async (data: SignUpForm) => {
+    try {
+      const userCrenditials = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      console.log({ userCrenditials });
+      await addDoc(collection(db, "users"), {
+        id: userCrenditials.user.uid,
+        fisrtName: data.firstName,
+        lastName: data.lastName,
+        email: userCrenditials.user.email,
+      });
+    } catch (error) {
+      const _error = error as AuthError;
+
+      if (_error.code === AuthErrorCodes.EMAIL_EXISTS) {
+        return setError('email', {type: 'alreadyInUse'})
+      }
+      
+    }
+  };
 
   return (
     <div className="flex justify-center mt-40 h-full">
@@ -65,16 +90,22 @@ const CreateLogin = () => {
             {errors?.email?.type === "required" && (
               <ErrorMessage>Por favor digitar o email</ErrorMessage>
             )}
+            {errors?.email?.type === 'alreadyInUse' && (
+              <ErrorMessage>O e-mail já esta em uso!</ErrorMessage>
+            )}
           </div>
           <div className="w-full mt-5">
             <p className="font-semibold mb-2">Senha</p>
             <CustomInput
               type="password"
               placeholder="Digite sua senha"
-              {...register("password", { required: true })}
+              {...register("password", { required: true, minLength: 6 })}
             />
             {errors?.password?.type === "required" && (
               <ErrorMessage>Por favor digitar a senha</ErrorMessage>
+            )}
+            {errors?.password?.type === "minLength" && (
+              <ErrorMessage>A senha deve conter no minimo 6 digitos</ErrorMessage>
             )}
           </div>
           <div className="w-full mt-5">
@@ -82,10 +113,13 @@ const CreateLogin = () => {
             <CustomInput
               placeholder="Confirmar senha"
               type="password"
-              {...register("passwordConfirmation")}
+              {...register("passwordConfirmation", {required: true, minLength: 6})}
             />
             {errors?.passwordConfirmation?.type === "required" && (
               <ErrorMessage>Por favor Confirme sua senha</ErrorMessage>
+            )}
+            {errors?.passwordConfirmation?.type === "minLength" && (
+              <ErrorMessage>A senha deve conter no minimo 6 digitos</ErrorMessage>
             )}
           </div>
           <Button startIcon={<FiLogIn />}>Criar Conta</Button>
