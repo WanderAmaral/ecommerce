333892161ab786278c70137691041a0f04cbe7fc
Wander Amaral <wanderamaral013@gmail.com>
2023-12-28 18:15:21 -0300

    feat: add condition if user was in login or logout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,17 +2,18 @@
 import { FunctionComponent, useEffect } from "react";
 import Categories from "./components/categories.component";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "@/config/firebase-config";
+import { auth, db } from "@/config/firebase-config";
 import { useContext } from "react";
 import { UserContext } from "@/contexts/user.context";
 import Button from "../components/button";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 
 const Home: FunctionComponent = () => {
   const { isAdministrador, isAuthenticated, loginUser, logoutUser } = useContext(UserContext);
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    onAuthStateChanged(auth, async (user) => {
       // se o usuário estiver logado no contexto, e o usuário do firebase (sign out)
       // devemos limpar o contexto (sign out) 
       console.log(user);
@@ -20,16 +21,17 @@ const Home: FunctionComponent = () => {
       if(isSignInOut) {
         return logoutUser()
       }
-      const isSignIngIn = !isAuthenticated %% user
+      const isSignIngIn = !isAuthenticated && user
       if(isSignIngIn) {
-        const querySnapshot = async () => {
-          
-        } 
-        return loginUser()
+        const querySnapshot = await getDocs(query(collection(db, 'users'), where('id', '==', user.uid)))
+
+        const userFromFirestore = querySnapshot.docs[0]?.data()
+        return loginUser(userFromFirestore as any)
       }
       
     });
   },);
+  console.log({isAuthenticated})
 
 
 
--- src/app/components/header.components.tsx
+++ src/app/components/header.components.tsx
@@ -1,52 +1,44 @@
 "use client";
 
 import { auth } from "@/config/firebase-config";
-import { reload, signOut } from "firebase/auth";
+import { UserContext } from "@/contexts/user.context";
+import { signOut } from "firebase/auth";
 import Link from "next/link";
+import { useContext } from "react";
 
 import { BsCart } from "react-icons/bs";
 
 const Header = () => {
-  
-  const handleClickSignOut = () => {
-    signOut(auth) 
-    window.location.reload()
-    window.location.href = "/"
-  }
+  const { isAuthenticated } = useContext(UserContext);
 
+  const handleClickSignOut = () => {
+    signOut(auth);
+    window.location.reload();
+    window.location.href = "/";
+  };
 
   return (
     <div className="  bg-background-dark w-full flex justify-between p-4 text-white shadow-xl">
-      {" "}
-      {/* Header-container */}
       <div className="flex items-center">
-        {" "}
-        {/* Header-items-container */}
         <Link href={"/"}>
           <h1 className="text-lg mr-4">Ecommerce</h1>
         </Link>{" "}
-        {/* header-title */}
       </div>
       <div className="flex items-center gap-10">
-        {" "}
-        {/* Header-items */}
-        <div className="flex font-semibold text-sm">
+        <div className="flex font-semibold text-sm cursor-pointer">
           <BsCart size={20} />
           <p className="ml-1">5</p>
-        </div>{" "}
-        {/* Header-item */}
-        <div className="font-semibold text-sm cursor-pointer">
-          Explorar
-        </div>{" "}
-        {/* Header-item */}
-        <Link href={"/login"}>
+        </div>
+        <div className="font-semibold text-sm cursor-pointer">Explorar</div>{" "}
+        {!isAuthenticated && (<Link href={"/login"}>
           <div className="font-semibold text-sm">Login</div>
-        </Link>{" "}
-        {/* Header-item */}
-      
-        {/* Header-item */}
-        <div className="font-semibold text-sm cursor-pointer" onClick={handleClickSignOut}>Sair</div>{" "}
-        {/* Header-item */}
+        </Link>)}
+        {isAuthenticated === true && <div
+          className="font-semibold text-sm cursor-pointer"
+          onClick={handleClickSignOut}
+        >
+          Sair
+        </div>}
       </div>
     </div>
   );
