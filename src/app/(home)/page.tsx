0299d82369da06fc2a6b272144a5813875eedf2c
Wander Amaral <wanderamaral013@gmail.com>
2023-12-28 17:48:24 -0300

    Update page.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,18 +9,27 @@ import Button from "../components/button";
 
 
 const Home: FunctionComponent = () => {
-  const { isAdministrador,isAuthenticated, loginUser, logoutUser } = useContext(UserContext);
+  const { isAdministrador, isAuthenticated, loginUser, logoutUser } = useContext(UserContext);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
+      // se o usuário estiver logado no contexto, e o usuário do firebase (sign out)
+      // devemos limpar o contexto (sign out) 
       console.log(user);
       const isSignInOut = isAuthenticated && !user
       if(isSignInOut) {
         return logoutUser()
       }
+      const isSignIngIn = !isAuthenticated %% user
+      if(isSignIngIn) {
+        const querySnapshot = async () => {
+          
+        } 
+        return loginUser()
+      }
       
     });
-  }, []);
+  },);
 
 
 
