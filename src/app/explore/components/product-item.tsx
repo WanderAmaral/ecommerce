d6d9ff943e9bd1b056681dcd9c7131d43dc87238
Wander Amaral <wanderamaral013@gmail.com>
2024-01-11 20:07:41 -0300

    fix: width image from component Prodcut Item

--- src/app/explore/components/product-item.tsx
+++ src/app/explore/components/product-item.tsx
@@ -9,7 +9,7 @@ interface ProductItemProps {
 const ProductItem: FunctionComponent<ProductItemProps> = ({product}) => {
     return ( 
         <div className="flex flex-col">
-            <div className=" h-96 w-72 bg-cover bg-no-repeat bg-center rounded-lg shadow-xl"><Image alt={product.name} src={product.imageUrl} width={0} height={0} sizes="100vh"/></div>
+            <div className=" h-96 w-72 bg-cover bg-no-repeat bg-center rounded-lg shadow-xl"><Image alt={product.name} src={product.imageUrl} width={200} height={100} sizes="100vh"/></div>
             <div className="flex justify-between mt-5 ">
                 <p className=" text-base font-medium">{product.name}</p>
                 <p className=" text-base font-medium">R$: {product.price}</p>
