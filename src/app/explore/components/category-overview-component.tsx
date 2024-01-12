294c425e8b378dbbc54d5ba4b38fbf15618bf9b4
Wander Amaral <wanderamaral013@gmail.com>
2024-01-12 15:34:06 -0300

    commit

--- src/app/explore/components/category-overview-component.tsx
+++ src/app/explore/components/category-overview-component.tsx
@@ -13,7 +13,7 @@ const CategoryOverview: FunctionComponent<CategoryOverviewProps> = ({category}) 
     return ( 
         <div className="flex flex-col w-full mt-5">
                 <p className=" text-xl font-medium mb-1">{category.displayName}</p>
-                <div className="flex w-full justify-between flex-wrap ">{category.products.slice(0, 4).map((product) => <ProductItem key={product.id} product={product}/>)}</div>
+                <div className="flex justify-between flex-wrap ">{category.products.slice(0, 4).map((product) => <ProductItem key={product.id} product={product}/>)}</div>
         </div>
      );
 }
--- src/app/explore/components/product-item.tsx
+++ src/app/explore/components/product-item.tsx
@@ -1,6 +1,7 @@
 import Product from "@/types/products.type";
 import { FunctionComponent } from "react";
 import Image from "next/image";
+import Button from "@/app/components/button/button";
 
 interface ProductItemProps {
     product: Product
@@ -9,7 +10,9 @@ interface ProductItemProps {
 const ProductItem: FunctionComponent<ProductItemProps> = ({product}) => {
     return ( 
         <div className="flex flex-col">
-            <div className=" h-96 w-72 bg-cover bg-no-repeat bg-center rounded-lg shadow-xl"><Image alt={product.name} src={product.imageUrl} width={200} height={100} sizes="100vh"/></div>
+            <div className=" h-96 w-72 bg-cover bg-no-repeat bg-center rounded-lg shadow-xl">
+            <Image alt={product.name} src={product.imageUrl} width={100} height={0} sizes="100vh"/>
+            <Button>Adicionar ao carrinho</Button></div>
             <div className="flex justify-between mt-5 ">
                 <p className=" text-base font-medium">{product.name}</p>
                 <p className=" text-base font-medium">R$: {product.price}</p>
