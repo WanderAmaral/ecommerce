1baa28899a6b10126db93b594980789c91bcd99c
Wander Amaral <wanderamaral013@gmail.com>
2024-04-10 14:34:39 -0300

    add identation and fix style

--- src/app/cart-item/cart-item-component.tsx
+++ src/app/cart-item/cart-item-component.tsx
@@ -4,39 +4,44 @@ import Button from "../components/button/button";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
 interface CartProductProps {
-   product: CartProduct
+  product: CartProduct;
 }
 
-const CartItemComponent:FunctionComponent<CartProductProps> = ({product}) => {
-    return ( <div className="flex flex-col items-center mb-4 text-black">
-        {/* container */}
-                <div className=" h-64 w-44 block rounded-lg" style={{backgroundImage: `url('${product.imageUrl}')`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
-                    {/* image */}
-                </div>
-                <div className="flex flex-col ml-5 items-center">
-                    {/* content */}
-                        <p className="text-xl font-semibold">
-                            {product.name}
-                        </p>
-                        <p>
-                            R$: {product.price}
-                        </p>
-                        <p>Quantidade: {product.quantity}</p>
-                    
-                    <div>
-                        {/* quantity */}
-                    </div>
-                </div>
-                <div className="flex gap-5">
-                    {/* remove Button */}
-                    
-                    <BsChevronLeft size={25}/>
-                    
-                    
-                    <BsChevronRight size={25}/>
-                    
-                </div>
-        </div> );
-}
- 
+const CartItemComponent: FunctionComponent<CartProductProps> = ({
+  product,
+}) => {
+  return (
+    <div className="flex  items-center mb-4 text-black">
+      {/* container */}
+      <div
+        className=" h-56 w-44 block rounded-lg"
+        style={{
+          backgroundImage: `url('${product.imageUrl}')`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        {/* image */}
+      </div>
+      <div className="flex flex-col ml-5">
+        {/* content */}
+        <p className="text-lg font-semibold">{product.name}</p>
+        <p className="mt-4">R$: {product.price}</p>
+        <p className="mt-4">Quantidade: {product.quantity}</p>
+
+        <div>{/* quantity */}</div>
+
+        <div className="flex gap-3 justify-center mt-3">
+          {/* remove Button */}
+
+          <BsChevronLeft size={25} />
+
+          <BsChevronRight size={25} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default CartItemComponent;
