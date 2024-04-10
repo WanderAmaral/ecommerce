4cc79d3bcdd3d463b33204a8e9e6fbb5123d8b0f
Wander Amaral <wanderamaral013@gmail.com>
2024-04-10 14:33:26 -0300

    created component selector

---
+++ src/app/store/reducers/cart/cart-selector.ts
@@ -0,0 +1,13 @@
+import { RootState } from "../../store";
+
+export const selectProductTotalPrice = (state: RootState) => {
+  return state.cartReducer?.products.reduce((acc, currentProduct) => {
+    return acc + currentProduct.price * currentProduct.quantity;
+  }, 0);
+};
+
+export const selectProductCount = (state: RootState) => {
+    return state.cartReducer?.products.reduce((acc, currentUser) => {
+        return acc + currentUser.quantity
+    }, 0)
+}
