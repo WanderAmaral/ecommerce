fca181826c3d5faa94b9bf163529f6086a8d9d5e
Wander Amaral <wanderamaral013@gmail.com>
2024-04-10 23:48:46 -0300

    fix: images to page explorer

--- src/app/explore/components/product-item.tsx
+++ src/app/explore/components/product-item.tsx
@@ -1,33 +1,39 @@
 import Product from "@/types/products.type";
-import { FunctionComponent} from "react";
+import { FunctionComponent } from "react";
 import Image from "next/image";
 import { useDispatch } from "react-redux";
 import { addProductToCart } from "@/app/store/reducers/cart/cart.action";
 
 interface ProductItemProps {
-    product: Product
+  product: Product;
 }
 
-const ProductItem: FunctionComponent<ProductItemProps> = ({product}) => {
-    const dispatch = useDispatch()
+const ProductItem: FunctionComponent<ProductItemProps> = ({ product }) => {
+  const dispatch = useDispatch();
 
-    const handleClickAddProduct = () => {
-        dispatch(addProductToCart(product))
-        
-    }
- 
-    return ( 
-        <div className="flex flex-col">
-            <div className=" h-96 w-56  bg-cover bg-no-repeat bg-center rounded-lg shadow-xl cursor-pointer" onClick={handleClickAddProduct}>
-            <Image alt={product.name} src={product.imageUrl} width={100} height={100} sizes="100vh" className="h-full w-full" />
-            <div className="flex justify-between">
-                <p className=" text-base font-medium">{product.name}</p>
-                <p className=" text-base font-medium">R$: {product.price}</p>
-            </div>
-           </div>
-            
-        </div>
-     );
-}
- 
+  const handleClickAddProduct = () => {
+    dispatch(addProductToCart(product));
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div
+        className=" h-96 w-56 rounded-lg shadow-xl cursor-pointer hover:opacity-70"
+        onClick={handleClickAddProduct}
+        style={{
+          backgroundImage: `url('${product.imageUrl}')`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+
+      <div className="flex flex-col ">
+        <p className=" text-[16px] font-bold">{product.name}</p>
+        <p className=" text-lg font-medium">R$: {product.price}</p>
+      </div>
+    </div>
+  );
+};
+
 export default ProductItem;
