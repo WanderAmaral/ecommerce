cbe82b966aac2c25c33c7a3578e721e449ab6d5d
Wander Amaral <wanderamaral013@gmail.com>
2024-08-08 17:02:14 -0300

    chore: dn tailwind and insert in component

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -1,21 +1,26 @@
+import { cn } from "@/lib/utils";
 import React, { FunctionComponent, ButtonHTMLAttributes } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   startIcon?: React.ReactNode;
   onClick?: () => void;
+  className?: string
 }
 
 const Button: FunctionComponent<ButtonProps> = ({
   children,
   startIcon,
   onClick,
+  className
 }) => {
+
+
   return (
-    <div>
+    <div className="w-full">
       <button
         onClick={onClick}
-        className="w-full rounded-lg justify-center  items-center bg-black p-3 text-texto-white text-sm  flex  font-semibold  transition ease-in duration-300"
+        className={cn("w-full rounded-r-lg justify-center  items-center bg-black p-3 text-texto-white text-sm  flex  font-semibold  transition ease-in duration-300", className)}
       >
         {startIcon && (
           <div className="flex h-full items-center mr-3">{startIcon}</div>
